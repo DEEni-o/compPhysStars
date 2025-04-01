0f23dd52b6d5d44aaf265965edb8d34e6532965c
DEEni-o <150621230+DEEni-o@users.noreply.github.com>
2025-04-01 15:30:47 +0100

    Removed coulomb force section due to it being redundant
    -Started trying to get gravity to work

---
+++ 4.py
@@ -0,0 +1,16 @@
+
+
+r = open("reactionsInfo.txt", "r")
+e = r.read()
+f = open("reactionsInfo.txt", "w")
+print(e)
+
+e = e.strip('[')
+e = e.replace("[", "")
+print(e)
+
+
+
+f.write(e)
+f.close()
+r.close()
